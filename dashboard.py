f2557d9a9ebd04e5f8fcae746c7980a14cd3b873
Jov-dot8 <114038455+Jov-dot8@users.noreply.github.com>
2023-05-03 18:27:50 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,56 +1,41 @@
 import streamlit as st
-import plotly.express as px
+import csv
 
-st.set_page_config(layout="wide")
+# Function to convert CSV to DAT
+def convert_csv_to_dat(csv_file):
+    with open(csv_file.name, 'r') as file:
+        # Read the CSV file
+        csv_reader = csv.reader(file)
 
-# -- Create three columns
-col1, col2, col3 = st.columns([5, 5, 20])
-# -- Put the image in the middle column
-# - Commented out here so that the file will run without having the image downloaded
-# with col2:
-# st.image("streamlit.png", width=200)
-# -- Put the title in the last column
-with col3:
-    st.title("Streamlit Demo")
-# -- We use the first column here as a dummy to add a space to the left
+        # Create a new file for the DAT file
+        dat_file = csv_file.name.replace('.csv', '.dat')
+        with open(dat_file, 'w') as new_file:
+            # Write each row to the new file
+            for row in csv_reader:
+                new_file.write('|'.join(row) + '\n')
 
-# -- Get the user input
-year_col, continent_col, log_x_col = st.columns([5, 5, 5])
-with year_col:
-    year_choice = st.slider(
-        "What year would you like to examine?",
-        min_value=1952,
-        max_value=2007,
-        step=5,
-        value=2007,
-    )
-with continent_col:
-    continent_choice = st.selectbox(
-        "What continent would you like to look at?",
-        ("All", "Asia", "Europe", "Africa", "Americas", "Oceania"),
-    )
-with log_x_col:
-    log_x_choice = st.checkbox("Log X Axis?")
+    return dat_file
 
-# -- Read in the data
-df = px.data.gapminder()
-# -- Apply the year filter given by the user
-filtered_df = df[(df.year == year_choice)]
-# -- Apply the continent filter
-if continent_choice != "All":
-    filtered_df = filtered_df[filtered_df.continent == continent_choice]
+# Main Streamlit app code
+def main():
+    # Set the title and subtitle of the app
+    st.title('CSV to DAT Converter')
+    st.subheader('Convert a CSV file to a DAT file')
 
-# -- Create the figure in Plotly
-fig = px.scatter(
-    filtered_df,
-    x="gdpPercap",
-    y="lifeExp",
-    size="pop",
-    color="continent",
-    hover_name="country",
-    log_x=log_x_choice,
-    size_max=60,
-)
-fig.update_layout(title="GDP per Capita vs. Life Expectancy")
-# -- Input the Plotly chart to the Streamlit interface
-st.plotly_chart(fig, use_container_width=True)
+    # Allow the user to upload a CSV file
+    uploaded_file = st.file_uploader('Choose a CSV file')
+
+    # If a file has been uploaded
+    if uploaded_file is not None:
+        # Show the name of the file
+        st.write('File name:', uploaded_file.name)
+
+        # Convert the CSV file to a DAT file
+        dat_file = convert_csv_to_dat(uploaded_file)
+
+        # Allow the user to download the DAT file
+        st.download_button('Download DAT file', dat_file)
+
+# Run the app
+if __name__ == '__main__':
+    main()
